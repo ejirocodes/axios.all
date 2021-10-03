1af99de227ecc5c7ace21934a15750761dfc4b32
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-10-03 17:42:46 +0100

    add methods for concurent request (Promise.all)

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ let endpoints = [
   'https://api.github.com/users/ejirocodes/following'
 ];
 
+function getUserAccount() {
+  return axios.get('https://reqres.in/api/users');
+}
+
+function getUserPermissions(id) {
+  return axios.get(`https://reqres.in/api/users/${id}`);
+}
+
+Promise.all([getUserAccount, getUserPermissions(4)]).then(
+  axios.spread((...allData) => {
+    console.log({ allData });
+  })
+);
+// The promise.all method returns a promise that resolves when all of the promises in the iterable argument have resolved.
+
+Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+  axios.spread((...allData) => {
+    console.log({ allData });
+  })
+);
+
+Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+   axios.spread(({data: user}, {data:repos}, {data:followers}, {data:following}) => {
+    console.log({ user, repos, followers, following });
+  })
+);
+
 Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(([{data: user}, {data: repos}, {data: followers}, {data: following}] )=> {
       console.log({ user, repos, followers, following });
 });
